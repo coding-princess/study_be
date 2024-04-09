08a13a0446f0ca062089c0243f911b9a4e0d3347
coding-princess <pyykity01@naver.com>
2024-04-09 16:53:16 +0900

    add post

---
+++ src/entities/post.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from './user.entity';
+
+@Entity()
+export class PostEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column({ default: 0 })
+  likes: number;
+
+  @ManyToOne(() => UserEntity, { onDelete: 'SET NULL' })
+  author: UserEntity;
+}
